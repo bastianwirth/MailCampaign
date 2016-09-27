732c5c838503d2ac3e37913095da362e6c28fca9
Bastian Wirth <bastianwirth@bastiansmacbook.guest.hbg.ssc.com>
2016-09-27 16:56:02 +0200

    Initial Commit

---
+++ MailCampaign/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MailCampaign
+//
+//  Created by Bastian Wirth on 27/09/2016.
+//  Copyright © 2016 Bastian Wirth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MailCampaign/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MailCampaign
+//
+//  Created by Bastian Wirth on 27/09/2016.
+//  Copyright © 2016 Bastian Wirth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
